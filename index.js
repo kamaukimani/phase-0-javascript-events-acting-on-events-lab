e199f684ea2c706f71568713d3251b186d4a41fd
kamaukimani <paulkimys285@gmail.com>
2025-06-25 12:37:25 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const game= document.getElementById("dodger");
+
+function moveDodgerLeft(){
+   
+   game.style.backgroundColor =  "red";
+   const leftNumbers = game.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+   if (left > 0){
+    game.style.left = `${left - 1}px`;
+
+   }
+
+}
+game.addEventListener("keydown", function(event){
+    if(event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+})
+function moveDodgerRight(){
+    const rightNumbers = game.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    if (left >= 0){
+        game.style.left = `${left + 1}px`;
+    }
+}
+game.addEventListener("keydown", function(event){
+    if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
